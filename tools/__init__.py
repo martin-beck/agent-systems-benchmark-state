7d5b527fec1dd931dc7edfb052d50106f739177f
Martin Beck <martin.beck2@gmx.de>
2026-09-14 21:10:10 +0200

    fix(state): make vendored coordinator package-checkable
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

---
+++ tools/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+"""Project tooling package for static analysis; command-line entry points remain direct scripts."""
